4358a81ca85a3dbd2aa95afc921e2e45617ae90f
h13i32maru <h13i32maru@gmail.com>
2016-06-05 23:45:36 +0900

    fix: releases

---
+++ js/track.js
@@ -0,0 +1,11 @@
+var trackEls = document.querySelectorAll('.js-track');
+for (var i = 0; i < trackEls.length; i++) {
+  trackEls[i].addEventListener('click', function(ev){
+    var url = ev.currentTarget.href;
+    ga('send', 'event', 'Download', 'click', url, {
+      'transport': 'beacon',
+      'hitCallback': function(){document.location = url;}
+    });
+    ev.preventDefault();
+  });
+}
